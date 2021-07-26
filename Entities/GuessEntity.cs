b46f9567d26fde8f9b43aa7e94b2b082601afb6c
Steven Yackel <styackel@microsoft.com>
2021-07-26 10:09:17 -0500

    Adds inner tile styling

---
+++ Entities/GuessEntity.cs
@@ -0,0 +1,16 @@
+﻿using CloudStorage.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PicturePanels.Entities
+{
+    public class GuessEntity
+    {
+        [Required]
+        [MinLength(1)]
+        public string Guess { get; set; }
+    }
+}
