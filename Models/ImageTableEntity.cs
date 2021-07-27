b8eb5a2120b85c05187fc0b5bf0d4559f4bbcdb5
Steven Yackel <styackel@microsoft.com>
2021-07-27 13:45:50 -0500

    adds better levenstien distance

--- Models/ImageTableEntity.cs
+++ Models/ImageTableEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Azure.Cosmos.Table;
 
 namespace PicturePanels.Models
@@ -27,7 +28,7 @@ namespace PicturePanels.Models
 
         public string Name { get; set; }
 
-        public List<string> AlternativeNames { get; set; }
+        public List<string> Answers { get; set; }
 
         public string UploadedBy { get; set; }
 
@@ -42,6 +43,28 @@ namespace PicturePanels.Models
         public ImageTableEntity Clone()
         {
             return (ImageTableEntity)this.MemberwiseClone();
+        }
+
+        public override void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            base.ReadEntity(properties, operationContext);
+
+            if (properties.ContainsKey(nameof(this.Answers)))
+            {
+                this.Answers = properties[nameof(this.Answers)].StringValue.Split(",", StringSplitOptions.RemoveEmptyEntries).ToList();
+            }
+        }
+
+        public override IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            var result = base.WriteEntity(operationContext);
+
+            if (this.Answers != null)
+            {
+                result[nameof(this.Answers)] = new EntityProperty(string.Join(",", this.Answers));
+            }
+
+            return result;
         }
     }
 }
