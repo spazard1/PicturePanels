57277faac896de85689cc5ceefce2a159e394b5b
Steven Yackel <styackel@microsoft.com>
2021-07-28 16:01:50 -0500

    animation fixes

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -372,6 +372,26 @@ function reloadPage() {
     location.reload();
 }
 
+function animateCSS(element, animation, prefix = 'animate__') {
+    const node = document.querySelector(element);
+    node.classList.add(prefix + "animated");
+
+    // We create a Promise and return it
+    new Promise((resolve) => {
+        const animationName = `${prefix}${animation}`;
+        node.classList.add(`${prefix}animated`, animationName);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
+}
+
 var signalRTimeOut = false;
 async function startSignalRAsync(playerIdSuffix) {
     if (!document.getElementById("signalRConnectionState")) {
