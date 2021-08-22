c2ad5e59c2800c26c93f20191afa1ccb42133beb
Steven Yackel <spazard1@gmail.com>
2021-08-22 15:32:55 -0500

    add base service bus

---
+++ Services/GameStateQueueService.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace PicturePanels.Services
+{
+    public class GameStateQueueService
+    {
+        public ServiceBusSender Sender { get; }
+        public ServiceBusReceiver Receiver { get; }
+
+        public GameStateQueueService()
+        {
+            var client = new ServiceBusClient("Endpoint=sb://picturepanels.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=TJLwj1a8CdqLrFrJeBaAGuUwulEbe9GbhHwK8WhQGdQ=");
+
+            Sender = client.CreateSender("gamestateupdates");
+            Receiver = client.CreateReceiver("gamestateupdates");
+        }
+    }
+}
