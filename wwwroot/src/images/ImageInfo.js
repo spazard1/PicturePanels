337e8994223f7f3769194d264eda967e9723ad00
Steven Yackel <styackel@microsoft.com>
2021-10-07 17:26:44 -0500

    adds react

---
+++ wwwroot/src/images/ImageInfo.js
@@ -0,0 +1,14 @@
+﻿
+
+function ImageInfo(props) {
+
+    //var imageSrc = "images/thumbnails/" + props.id + "?" + props.queryString;
+
+    return (
+        <div className="imageInfo">
+            test
+        </div>
+    );
+}
+
+export default ImageInfo;
