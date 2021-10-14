5e0381a3094a6f33805184b2aeaaf0603cd0b122
Steven Yackel <styackel@microsoft.com>
2021-10-14 17:30:06 -0500

    themes

---
+++ Entities/ScoreChangeEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+
+namespace PicturePanels.Models
+{
+    public class GameRoundEntity : IComparable<GameRoundEntity>
+    {
+        public GameRoundEntity()
+        {
+
+        }
+
+        public GameRoundEntity(GameRoundTableEntity gameRoundTableEntity)
+        {
+            this.RoundNumber = gameRoundTableEntity.RoundNumber;
+            this.TeamOneScore = gameRoundTableEntity.TeamOneScore;
+            this.TeamTwoScore = gameRoundTableEntity.TeamTwoScore;
+        }
+
+        public int RoundNumber { get; set; }
+
+        public int TeamOneScore { get; set; }
+
+        public int TeamTwoScore { get; set; }
+
+        public int CompareTo(GameRoundEntity other)
+        {
+            return this.RoundNumber - other.RoundNumber;
+        }
+    }
+}
