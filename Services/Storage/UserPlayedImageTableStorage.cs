835ac276935b0e7f6469d4c72b6d1b6931605eaf
Steven Yackel <styackel@microsoft.com>
2022-02-25 16:36:20 -0600

    adds toggle played

--- Services/Storage/UserPlayedImageTableStorage.cs
+++ Services/Storage/UserPlayedImageTableStorage.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using PicturePanels.Entities;
 using PicturePanels.Models;
 
 namespace PicturePanels.Services.Storage
@@ -10,6 +12,41 @@ namespace PicturePanels.Services.Storage
         public UserPlayedImageTableStorage(ICloudStorageAccountProvider cloudStorageAccountProvider) : base(cloudStorageAccountProvider, "userplayedimages")
         {
 
+        }
+
+        public async IAsyncEnumerable<ImageEntity> PopulateIsPlayed(IAsyncEnumerable<ImageEntity> imageEntities, string userId)
+        {
+            var tasks = new List<Task<ImageEntity>>();
+            await foreach (var imageEntity in imageEntities)
+            {
+                var task = this.GetAsync(imageEntity.Id, userId).ContinueWith(userPlayedImageModel =>
+                {
+                    if (userPlayedImageModel.Result != null)
+                    {
+                        imageEntity.IsPlayed = true;
+                    }
+                    return imageEntity;
+                });
+
+                tasks.Add(task);
+
+                if (tasks.Count >= QueryBatchSize)
+                {
+                    await Task.WhenAll(tasks);
+
+                    foreach (var imageEntityTask in tasks)
+                    {
+                        yield return imageEntityTask.Result;
+                    }
+
+                    tasks.Clear();
+                }
+            }
+
+            foreach (var imageEntityTask in tasks)
+            {
+                yield return imageEntityTask.Result;
+            }
         }
     }
 }
