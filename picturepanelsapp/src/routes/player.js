ff5ee0c78007f11a5705e770180a2cc7eabb4499
Steven Yackel <styackel@microsoft.com>
2022-03-15 14:24:50 -0500

    set title

--- picturepanelsapp/src/routes/player.js
+++ picturepanelsapp/src/routes/player.js
@@ -1,4 +1,11 @@
+import React, { useEffect } from "react";
+
 export default function Player() {
+
+    useEffect(() => {
+        document.title = "Picture Panels";
+    }, []);
+
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Player</h2>
