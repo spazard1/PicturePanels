6b656e3db29627b389a595690dd332b5254127d5
Steven Yackel <styackel@microsoft.com>
2022-03-16 10:09:11 -0500

    link import

--- picturepanelsapp/src/routes/gameboard.js
+++ picturepanelsapp/src/routes/gameboard.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Gameboard() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Gameboard</h2>
+        <Link to="/">Player</Link>
       </main>
     );
   }
