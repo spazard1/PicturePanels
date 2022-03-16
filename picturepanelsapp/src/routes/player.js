c4d1f53fe91074e23a72ec70843d6b5a94622c3f
Steven Yackel <styackel@microsoft.com>
2022-03-16 10:02:01 -0500

    links

--- picturepanelsapp/src/routes/player.js
+++ picturepanelsapp/src/routes/player.js
@@ -9,6 +9,7 @@ export default function Player() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Player</h2>
+        <Link to="/gameboard">Gameboard</Link>
       </main>
     );
   }
