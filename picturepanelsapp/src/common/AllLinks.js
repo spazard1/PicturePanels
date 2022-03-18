59673a524d64aa4cf2fb9a859750f4090ea9b214
Steven Yackel <styackel@microsoft.com>
2022-03-18 12:38:11 -0500

    linter
    test linter
    
    test
    
    husky
    
    test

--- picturepanelsapp/src/common/AllLinks.js
+++ picturepanelsapp/src/common/AllLinks.js
@@ -2,12 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function AllLinks() {
-    return (
-        <>
-            <div><Link to="/">Player</Link></div>
-            <div><Link to="/gameboard">Gameboard</Link></div>
-            <div><Link to="/upload">Upload</Link></div>
-            <div><Link to="/admin">Admin</Link></div>
-        </>
-    );
-  }
+  return (
+    <>
+      <div>
+        <Link to="/">Player</Link>
+      </div>
+      <div>
+        <Link to="/gameboard">Gameboard</Link>
+      </div>
+      <div>
+        <Link to="/upload">Upload</Link>
+      </div>
+      <div>
+        <Link to="/admin">Admin</Link>
+      </div>
+    </>
+  );
+}
