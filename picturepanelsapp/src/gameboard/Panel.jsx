9844133fc4505f8fa42be316aa0d90f8ce987963
Steven Yackel <styackel@microsoft.com>
2022-03-21 16:09:54 -0500

    adds panels

--- picturepanelsapp/src/gameboard/Panel.jsx
+++ picturepanelsapp/src/gameboard/Panel.jsx
@@ -1,35 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 import "./Panel.css";
+import { GetExitClass } from "../animate/Animate";
 
 export default function Panel({
   isOpen,
   roundNumber,
   panelNumber,
-  animationClasses,
+  entranceClass,
 }) {
+  const [exitClass, setExitClass] = useState();
+
+  useEffect(() => {
+    setExitClass(GetExitClass());
+  }, [isOpen]);
+
   return (
     <div id={"panel_" + panelNumber} className="panel">
       <div
         className={
           "panelBackground animate__animated animate__slow " +
-          animationClasses.join(" ")
+          (isOpen ? exitClass : entranceClass)
         }
       >
         <div id={"panelNumber_" + panelNumber} className="panelNumber">
           {panelNumber}
         </div>
-        <img
-          className={"panelImage"}
-          src={
-            "https://picturepanels.azurewebsites.net/api/images/panels/KDML/" +
-            roundNumber +
-            "/" +
-            (isOpen ? panelNumber : 0)
-          }
-        />
       </div>
+      <img
+        className={"panelImage"}
+        src={
+          "https://picturepanels.azurewebsites.net/api/images/panels/KDML/" +
+          roundNumber +
+          "/" +
+          (isOpen ? panelNumber : 0)
+        }
+      />
     </div>
   );
 }
@@ -38,5 +45,5 @@ Panel.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   panelNumber: PropTypes.string.isRequired,
   roundNumber: PropTypes.number.isRequired,
-  animationClasses: PropTypes.arrayOf(PropTypes.string).isRequired,
+  entranceClass: PropTypes.string.isRequired,
 };
