c578b2d5f6b42b468f36ed738923762431931a2d
Steven Yackel <styackel@microsoft.com>
2022-03-21 15:30:07 -0500

    use real fetch

--- picturepanelsapp/src/gameboard/Panel.jsx
+++ picturepanelsapp/src/gameboard/Panel.jsx
@@ -3,18 +3,40 @@ import PropTypes from "prop-types";
 
 import "./Panel.css";
 
-export default function Panel({ panelNumber }) {
+export default function Panel({
+  isOpen,
+  roundNumber,
+  panelNumber,
+  animationClasses,
+}) {
   return (
     <div id={"panel_" + panelNumber} className="panel">
-      <div className="panelBackground animate__animated animate__slow">
+      <div
+        className={
+          "panelBackground animate__animated animate__slow " +
+          animationClasses.join(" ")
+        }
+      >
         <div id={"panelNumber_" + panelNumber} className="panelNumber">
           {panelNumber}
         </div>
+        <img
+          className={"panelImage"}
+          src={
+            "https://picturepanels.azurewebsites.net/api/images/panels/KDML/" +
+            roundNumber +
+            "/" +
+            (isOpen ? panelNumber : 0)
+          }
+        />
       </div>
     </div>
   );
 }
 
 Panel.propTypes = {
-  panelNumber: PropTypes.number.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  panelNumber: PropTypes.string.isRequired,
+  roundNumber: PropTypes.number.isRequired,
+  animationClasses: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
