2598968137b9aa4192af01a8071d013899d91c6a
Steven Yackel <styackel@microsoft.com>
2022-03-22 14:59:16 -0500

    panel measuring

---
+++ picturepanelsapp/src/common/usePlayers.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useSignalR } from "../signalr/useSignalR";
+import getPlayers from "./getPlayers";
+
+export function usePlayers(gameStateId) {
+  const [players, setPlayers] = useState({});
+
+  const connectionId = useSignalR("Players", (players) => {
+    setPlayers(players);
+  });
+
+  useSignalR("AddPlayer", (player) => {
+    const newPlayers = { ...players };
+    newPlayers[player.id] = player;
+    setPlayers(newPlayers);
+  });
+
+  useEffect(() => {
+    if (!gameStateId) {
+      return;
+    }
+
+    getPlayers(gameStateId, (players) => {
+      const newPlayers = players.reduce((aggregate, value) => ({ ...aggregate, [value.playerId]: value }), {});
+
+      setPlayers(newPlayers);
+    });
+  }, [gameStateId, connectionId]);
+
+  return players;
+}
