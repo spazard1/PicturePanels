b9a1da85c8e43067a82153e1be012170967b36ec
Jinseo Park <jinseopark@microsoft.com>
2022-03-21 20:59:47 -0700

    worked on TeamInfos skeletons but css needs to be fixed

---
+++ picturepanelsapp/src/teaminfos/ScoreBoard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./ScoreBoard.css";
+
+function ScoreBoard({
+  isTeamOnePlaying,
+  teamOneScore,
+  teamTwoScore,
+  isGamePaused,
+}) {
+  return (
+    <div className="scoreBoard">
+      <div className="scoreTeam">{teamOneScore}</div>
+      <div className="scoreBoardStatus">
+        {isGamePaused
+          ? "Game is paused"
+          : isTeamOnePlaying
+          ? "← Open a Panel"
+          : "Open a Panel →"}
+      </div>
+      <div className="scoreTeam">{teamTwoScore}</div>
+    </div>
+  );
+}
+
+ScoreBoard.propTypes = {
+  isTeamOnePlaying: PropTypes.bool.isRequired,
+  teamOneScore: PropTypes.number.isRequired,
+  teamTwoScore: PropTypes.number.isRequired,
+  isGamePaused: PropTypes.bool.isRequired,
+};
+
+export default ScoreBoard;
---
+++ picturepanelsapp/src/teaminfos/Team.jsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from "react";
+import PropTypes from "prop-types";
+import "./Team.css";
+
+function Team({ teamName, teamIncorrectGuesses, teamInnerPanels, isTeamOne }) {
+  const renderPanelCount = useCallback((n) => {
+    return [...Array(n)].map((_, i) => (
+      <div
+        key={i}
+        className={`panelCount ${isTeamOne ? "teamOneBox" : "teamTwoBox"}`}
+      ></div>
+    ));
+  }, []);
+
+  const renderIncorrectGuesses = useCallback((n) => {
+    return [...Array(n)].map((_, i) => (
+      <div key={i} className="teamInfoIncorrectGuesses">
+        ⦻
+      </div>
+    ));
+  }, []);
+
+  return (
+    <div className="teamInfo teamNameBox">
+      <div className="teamName">{teamName}</div>
+      <div className="teamInfoBottomContainer">
+        <div className="teamInfoIncorrectGuessesContainer">
+          {renderIncorrectGuesses(teamIncorrectGuesses)}
+        </div>
+        <div className="teamInfoPanelCounts">
+          {renderPanelCount(teamInnerPanels)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Team.propTypes = {
+  teamName: PropTypes.string.isRequired,
+  teamIncorrectGuesses: PropTypes.number.isRequired,
+  teamInnerPanels: PropTypes.number.isRequired,
+  isTeamOne: PropTypes.bool.isRequired,
+};
+
+export default Team;
