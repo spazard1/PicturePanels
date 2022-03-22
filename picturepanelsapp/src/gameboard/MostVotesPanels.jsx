ce97e3a586f3f58213341c667387538b36fe4de5
Steven Yackel <styackel@microsoft.com>
2022-03-22 17:40:18 -0500

    working on most votes panels

--- picturepanelsapp/src/gameboard/MostVotesPanels.jsx
+++ picturepanelsapp/src/gameboard/MostVotesPanels.jsx
@@ -1,23 +1,107 @@
-import React, { useEffect } from "react";
-import MostVotesPanel from "./MostVotesPanel";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 import "./MostVotesPanels.css";
 
-const mostVotesPanels = [...Array(3).keys()].map((panelNumber) => panelNumber + 1 + "");
+const MaxMostVotesPanels = 3;
 
-export default function MostVotesPanels(players) {
-  useEffect(() => {}, [players]);
+export default function MostVotesPanels({ panelRefs, players }) {
+  const [mostVotesPanelsRects, setMostVotesPanelsRects] = useState([]);
+
+  useEffect(() => {
+    console.log(panelRefs[1].current.src);
+
+    console.log(panelRefs[1].current.getBoundingClientRect().height);
+
+    const panelVotes = {};
+    for (var i = 1; i <= 20; i++) {
+      panelVotes[i + ""] = 0;
+    }
+
+    for (const playerId in players) {
+      let player = players[playerId];
+
+      if (player === 0) {
+        continue;
+      }
+
+      player.selectedPanels.forEach((panel) => {
+        panelVotes[panel]++;
+      });
+    }
+
+    let mostVotes = 0;
+    let mostVotesPanels = [];
+
+    for (const panel in panelVotes) {
+      var panelElement = document.getElementById("panel_" + panel);
+      if (panelElement.classList.contains("panelOpen")) {
+        continue;
+      }
+
+      if (panelVotes[panel] > mostVotes) {
+        mostVotes = panelVotes[panel];
+        mostVotesPanels = [panel];
+      } else if (panelVotes[panel] === mostVotes) {
+        mostVotesPanels.push(panel);
+      }
+    }
+
+    while (mostVotesPanels.length < MaxMostVotesPanels) {
+      mostVotesPanels.push(20 + mostVotesPanels.length);
+    }
+
+    let mostVotesPanelsRects = {};
+
+    for (const panelNumber in mostVotesPanels) {
+      if (panelNumber > 20) {
+        mostVotesPanelsRects[panelNumber] = null;
+      } else {
+        mostVotesPanelsRects[panelNumber] = panelRefs[panelNumber].current.getBoundingClientRect();
+      }
+    }
+
+    console.log(mostVotesPanelsRects);
+
+    setMostVotesPanelsRects(mostVotesPanelsRects);
+  }, [panelRefs, players]);
 
   return (
     <div id="mostVotesPanels">
-      {mostVotesPanels.map((mostVotesPanelNumber) => (
-        <MostVotesPanel key={mostVotesPanelNumber}></MostVotesPanel>
+      {Object.keys(mostVotesPanelsRects).map((panelNumber) => (
+        <>
+          {panelNumber > 20 && (
+            <div
+              key={panelNumber}
+              className="mostVotesPanel opacity0"
+              style="transform: translate(753.927px, 528.302px); width: 300.125px; height: 212.583px;"
+            ></div>
+          )}
+          {panelNumber <= 20 && (
+            <div
+              key={panelNumber}
+              className="mostVotesPanel"
+              style={
+                "transform: translate(" +
+                mostVotesPanelsRects[panelNumber].x +
+                "px, " +
+                mostVotesPanelsRects[panelNumber].y +
+                "px); " +
+                "width: " +
+                mostVotesPanelsRects[panelNumber].width +
+                "px; height: " +
+                mostVotesPanelsRects[panelNumber].height +
+                "px;"
+              }
+            ></div>
+          )}
+        </>
       ))}
     </div>
   );
 }
 
 MostVotesPanels.propTypes = {
+  panelRefs: PropTypes.arrayOf(PropTypes.object),
   players: PropTypes.object.isRequired,
 };
--- picturepanelsapp/src/gameboard/Panel.jsx
+++ picturepanelsapp/src/gameboard/Panel.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import "./Panel.css";
 import { GetExitClass } from "../animate/Animate";
 
-const Panel = ({ isOpen, roundNumber, panelNumber, entranceClass }, ref) => {
+const Panel = ({ gameStateId, isOpen, roundNumber, panelNumber, entranceClass, setImagesLoaded }, ref) => {
   const [exitClass, setExitClass] = useState();
 
   useEffect(() => {
@@ -12,16 +12,24 @@ const Panel = ({ isOpen, roundNumber, panelNumber, entranceClass }, ref) => {
   }, [isOpen]);
 
   return (
-    <div ref={ref} id={"panel_" + panelNumber} className="panel">
+    <div id={"panel_" + panelNumber} className="panel">
       <div className={"panelBackground animate__animated animate__slow " + (isOpen ? exitClass : entranceClass)}>
         <div id={"panelNumber_" + panelNumber} className="panelNumber">
           {panelNumber}
         </div>
       </div>
-      <img
-        className={"panelImage"}
-        src={"https://picturepanels.azurewebsites.net/api/images/panels/KDML/" + roundNumber + "/" + (isOpen ? panelNumber : 0)}
-      />
+      {gameStateId && (
+        <img
+          ref={ref}
+          className={"panelImage"}
+          onLoad={() => {
+            setImagesLoaded((imagesLoaded) => {
+              return { ...imagesLoaded, [panelNumber]: true };
+            });
+          }}
+          src={"https://picturepanels.azurewebsites.net/api/images/panels/" + gameStateId + "/" + roundNumber + "/" + (isOpen ? panelNumber : 0)}
+        />
+      )}
     </div>
   );
 };
@@ -29,8 +37,10 @@ const Panel = ({ isOpen, roundNumber, panelNumber, entranceClass }, ref) => {
 export default forwardRef(Panel);
 
 Panel.propTypes = {
+  gameStateId: PropTypes.string.isRequired,
   isOpen: PropTypes.bool.isRequired,
   panelNumber: PropTypes.string.isRequired,
   roundNumber: PropTypes.number.isRequired,
   entranceClass: PropTypes.string.isRequired,
+  setImagesLoaded: PropTypes.object.isRequired,
 };
