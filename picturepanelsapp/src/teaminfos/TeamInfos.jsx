a88085cb88c7207298286b99c076687a27920e9a
Steven Yackel <styackel@microsoft.com>
2022-03-23 13:37:33 -0500

    fix team errors

--- picturepanelsapp/src/teaminfos/TeamInfos.jsx
+++ picturepanelsapp/src/teaminfos/TeamInfos.jsx
@@ -9,17 +9,22 @@ function TeamInfos({ gameState }) {
     <>
       <div className="teamInfos">
         <Team
-          teamName={gameState.teamOneName}
-          teamIncorrectGuesses={gameState.teamOneIncorrectGuesses}
-          teamInnerPanels={gameState.teamOneInnerpanels}
+          teamName={gameState.teamOneName ?? "Team 1"}
+          teamIncorrectGuesses={gameState.teamOneIncorrectGuesses ?? 0}
+          teamInnerPanels={gameState.teamOneInnerPanels ?? 5}
           isTeamActive={gameState.teamTurn === 1 ? true : false}
           isTeamOne={true}
         />
-        <ScoreBoard isGamePaused={false} isTeamOnePlaying={true} teamOneScore={gameState.teamOneScore} teamTwoScore={gameState.teamTwoScore} />
+        <ScoreBoard
+          isGamePaused={false}
+          isTeamOnePlaying={true}
+          teamOneScore={gameState.teamOneScore ?? 0}
+          teamTwoScore={gameState.teamTwoScore ?? 0}
+        />
         <Team
-          teamName={gameState.teamTwoName}
-          teamIncorrectGuesses={gameState.teamTwoIncorrectGuesses}
-          teamInnerPanels={gameState.teamTwoInnerPanels}
+          teamName={gameState.teamTwoName ?? "Team 2"}
+          teamIncorrectGuesses={gameState.teamTwoIncorrectGuesses ?? 0}
+          teamInnerPanels={gameState.teamTwoInnerPanels ?? 5}
           isTeamActive={gameState.teamTurn === 2 ? true : false}
           isTeamOne={false}
         />
