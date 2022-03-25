94fdaabe85dd75a76d8e8157a0e00a59075a223f
Steven Yackel <styackel@microsoft.com>
2022-03-25 14:48:23 -0500

    remove logs

--- picturepanelsapp/src/teaminfos/TeamInfos.jsx
+++ picturepanelsapp/src/teaminfos/TeamInfos.jsx
@@ -5,7 +5,6 @@ import PropTypes from "prop-types";
 import "./TeamInfos.css";
 
 function TeamInfos({ gameState }) {
-  console.log(gameState);
   return (
     <>
       <div className="teamInfos">
