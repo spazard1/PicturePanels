38d31a91963140043f223cbb0b99b64e3e761e85
Steven Yackel <styackel@microsoft.com>
2022-03-25 14:50:07 -0500

    ignore warning:

--- picturepanelsapp/src/common/useImageLoader.jsx
+++ picturepanelsapp/src/common/useImageLoader.jsx
@@ -11,22 +11,19 @@ export function useImageLoader(imageRefs) {
 
     imageRefs.forEach((imageRef) => {
       imageRef.current.addEventListener("load", function () {
-        console.log(imagesLoaded);
         setImagesLoaded({ ...imagesLoaded, [imageRef.current.src]: true });
       });
       imageRef.current.addEventListener("loadstart", function () {
-        console.log("load starting");
         setAllImagesLoaded(false);
       });
     });
 
     return () => {
       imageRefs.foreach((imageRef) => {
-        imageRef.re("load", function (e) {
-          console.log("Image loaded", e);
-        });
+        imageRef.re("load", function () {});
       });
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [imageRefs]);
 
   return allImagesLoaded;
