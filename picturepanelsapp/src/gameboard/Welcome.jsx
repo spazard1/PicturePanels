18512e7e64d910bbf63e7a7da2a48ff9d6d37930
Steven Yackel <styackel@microsoft.com>
2022-04-06 14:45:54 -0500

    adds welcome screen

---
+++ picturepanelsapp/src/gameboard/Welcome.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import WelcomeJoinGame from "./WelcomeJoinGame";
+import PropTypes from "prop-types";
+
+import "./Welcome.css";
+
+const Welcome = ({ welcomeState, onWelcomeStateChange, onJoinGame, onCancel }) => {
+  return (
+    <div className="welcomeContainer">
+      {welcomeState === "" && (
+        <div className="welcomeText welcomeStartGame">
+          Welcome to Picture Panels!
+          <br />
+          <div className="center defaultButton welcomeButton">Create New Game</div>
+          <div
+            className="center defaultButton welcomeButton"
+            onClick={() => {
+              onWelcomeStateChange("Join");
+            }}
+          >
+            Join Existing Game
+          </div>
+          <div className="loginContainer center">
+            <div className="center defaultButton welcomeButton">Login</div>
+            <div className="center loginMessage">When you are logged in, you will not see images that you have already played.</div>
+            <a href="newuser">Create a new user</a>
+          </div>
+          <div className="loggedInUser center">
+            Logged in as: <span className="loggedInUserDisplayName"></span>
+            <br />
+            <input type="button" value="Logout" className="loggedInUserButton" />
+          </div>
+        </div>
+      )}
+      {welcomeState === "Join" && <WelcomeJoinGame onCancel={onCancel} onJoinGame={onJoinGame}></WelcomeJoinGame>}
+    </div>
+  );
+};
+
+export default Welcome;
+
+Welcome.propTypes = {
+  welcomeState: PropTypes.string.isRequired,
+  onWelcomeStateChange: PropTypes.func.isRequired,
+  onJoinGame: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
