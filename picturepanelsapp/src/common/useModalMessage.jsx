6bb5e3974d33e0f700b8ccb6831570b51d2b6a94
Steven Yackel <spazard1@gmail.com>
2022-04-17 20:07:34 -0500

    adds player changes

---
+++ picturepanelsapp/src/common/useModalMessage.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export function useModalMessage() {
+  const [modalMessage, setModalMessage] = useState();
+
+  const onModalClose = () => {
+    setModalMessage("");
+  };
+
+  return { modalMessage, setModalMessage, onModalClose };
+}
