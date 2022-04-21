173864ac0004a9d5ff949ed032edcf0881775632
Steven Yackel <styackel@microsoft.com>
2022-04-21 13:50:56 -0500

    player choose team

--- picturepanelsapp/src/player/ChooseTeam.jsx
+++ picturepanelsapp/src/player/ChooseTeam.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./ChooseTeam.css";
+import getSmallestTeam from "./getSmallestTeam";
 
-const ChooseTeam = ({ teamOneName, teamTwoName }) => {
+const ChooseTeam = ({ gameStateId, teamOneName, teamTwoName, onTeamNumberChange }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const chooseSmallestTeamOnClick = () => {
+    if (isLoading) {
+      return;
+    }
+
+    setIsLoading(true);
+    getSmallestTeam(gameStateId, (teamNumber) => {
+      setIsLoading(false);
+      onTeamNumberChange(teamNumber);
+    });
+  };
+
+  const onTeamNumberChangeClick = (teamNumber) => {
+    if (isLoading) {
+      return;
+    }
+    onTeamNumberChange(teamNumber);
+  };
+
   return (
     <>
-      <div className="chooseTeam center">Choose your team</div>
-      <div className="playerBanner playerBannerChooseTeam center">
-        <div className="playerBannerItem playerName"></div>
-        <div className="playerBannerItem">
-          <div className="teamName chooseTeamName teamBox teamOneBox">{teamOneName}</div>
-          <div className="teamName chooseTeamName teamBox teamTwoBox">{teamTwoName}</div>
-          <div className="teamName chooseTeamName chooseSmallestTeamBox">Choose for me</div>
-        </div>
+      <div className="chooseTeamLabel center">Choose your team</div>
+      <div className="chooseTeamNames">
+        {!isLoading && (
+          <>
+            <div className="chooseTeamName teamBox teamOneBox center" onClick={() => onTeamNumberChangeClick(1)}>
+              {teamOneName}
+            </div>
+            <div className="chooseTeamName teamBox teamTwoBox center" onClick={() => onTeamNumberChangeClick(2)}>
+              {teamTwoName}
+            </div>
+            <div className="chooseTeamName chooseSmallestTeamBox center" onClick={chooseSmallestTeamOnClick}>
+              {isLoading ? "Loading" : "Choose for me"}
+            </div>
+          </>
+        )}
       </div>
     </>
   );
@@ -22,6 +51,8 @@ const ChooseTeam = ({ teamOneName, teamTwoName }) => {
 export default ChooseTeam;
 
 ChooseTeam.propTypes = {
+  gameStateId: PropTypes.string.isRequired,
   teamOneName: PropTypes.string.isRequired,
   teamTwoName: PropTypes.string.isRequired,
+  onTeamNumberChange: PropTypes.func.isRequired,
 };
--- picturepanelsapp/src/player/Player.jsx
+++ picturepanelsapp/src/player/Player.jsx
@@ -12,6 +12,7 @@ import JoinGame from "./JoinGame";
 import MessageModal from "../common/ModalMessage";
 import { useModalMessage } from "../common/useModalMessage";
 import "./Player.css";
+import putPlayer from "./putPlayer";
 
 export default function Player() {
   useBodyClass("player");
@@ -19,6 +20,8 @@ export default function Player() {
   const { queryString, setQueryString } = useSignalRConnection();
   const { modalMessage, setModalMessage, onModalClose } = useModalMessage();
   const [isLoading, setIsLoading] = useState(false);
+  const [player, setPlayer] = useState();
+  const [teamNumber, setTeamNumber] = useState();
   const { gameState, gameStateId, setGameState } = useGameState();
 
   let initialColor;
@@ -54,6 +57,38 @@ export default function Player() {
     localStorage.setItem("playerColor", color);
   };
 
+  const onTeamNumberChange = (teamNumber) => {
+    if (!teamNumber) {
+      setModalMessage("Could not join the game. Refresh the page and try again.");
+    } else {
+      setTeamNumber(teamNumber);
+    }
+  };
+
+  useEffect(() => {
+    if (!gameStateId || !teamNumber || player) {
+      return;
+    }
+
+    putPlayer(
+      gameStateId,
+      {
+        PlayerId: localStorage.getItem("playerId"),
+        Name: localStorage.getItem("playerName"),
+        TeamNumber: teamNumber,
+        Color: color,
+      },
+      (p) => {
+        if (p) {
+          setPlayer(p);
+          localStorage.setItem("playerId", p.playerId);
+        } else {
+          setModalMessage("Could not join the game. Refresh the page and try again.");
+        }
+      }
+    );
+  }, [gameStateId, teamNumber, color, player, setModalMessage]);
+
   useEffect(() => {
     if (!gameState || !gameStateId) {
       return;
@@ -65,21 +100,32 @@ export default function Player() {
   }, [gameStateId, gameState, queryString, setQueryString]);
 
   return (
-    <div className="main center">
+    <div className="main center flexColumns">
       <MessageModal modalMessage={modalMessage} onModalClose={onModalClose}></MessageModal>
 
-      <JoinGame color={color} isLoading={isLoading} onJoinGame={onJoinGame} onColorChange={onColorChange}></JoinGame>
+      {!gameState && <JoinGame color={color} isLoading={isLoading} onJoinGame={onJoinGame} onColorChange={onColorChange}></JoinGame>}
 
-      {gameState && <ChooseTeam teamOneName={gameState.teamOneName} teamTwoName={gameState.teamTwoName}></ChooseTeam>}
+      {gameState && !teamNumber && (
+        <ChooseTeam
+          gameStateId={gameState.gameStateId}
+          teamOneName={gameState.teamOneName}
+          teamTwoName={gameState.teamTwoName}
+          onTeamNumberChange={onTeamNumberChange}
+        ></ChooseTeam>
+      )}
 
-      <div className="turnStatusMessage center"></div>
+      {player && (
+        <>
+          <div className="turnStatusMessage center"></div>
 
-      {gameState && gameState.turnType === "Welcome" && <StartGameButtons turnEndTime={gameState.turnEndTime}></StartGameButtons>}
+          {gameState && gameState.turnType === "Welcome" && <StartGameButtons turnEndTime={gameState.turnEndTime}></StartGameButtons>}
 
-      {gameState && <PanelButtons></PanelButtons>}
-      {gameState && <TeamGuesses></TeamGuesses>}
+          {gameState && <PanelButtons></PanelButtons>}
+          {gameState && <TeamGuesses></TeamGuesses>}
 
-      <Chat></Chat>
+          <Chat></Chat>
+        </>
+      )}
     </div>
   );
 }
